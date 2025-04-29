751b2f155dfcd3bd98c44a7cc4d3255a893a575f
Madiyar <wolfenstrun@gmail.com>
2025-04-29 23:56:29 +0500

    init commit BST and Testing done

--- src/BST.java
+++ src/BST.java
@@ -1,7 +1,27 @@
-import org.w3c.dom.Node;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
 
-public class BST<K extends Comparable<K>, V> {
+public class BST<K extends Comparable<K>, V> implements Iterable<BST.Element<K, V>> {
     private Node root;
+    private int size;
+
+    public static class Element<K, V> {
+        private final K key;
+        private final V value;
+        public Element(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public K getKey() {
+            return key;
+        }
+        public V getValue() {
+            return value;
+        }
+    }
+
     private class Node {
         private K key;
         private V val;
@@ -11,17 +31,89 @@ public class BST<K extends Comparable<K>, V> {
             this.val = val;
         }
     }
-    public void put(K key, V val) {}
+
+    public int size() {
+        return size;
+    }
+
+    public void put(K key, V val) {
+        if (root == null) {
+            root = new Node(key, val);
+            size++;
+            return;
+        }
+        Node current = root;
+        Node parent = null;
+        int cmp = 0;
+        while (current != null) {
+            parent = current;
+            cmp = key.compareTo(current.key);
+            if (cmp < 0) current = current.left;
+            else if (cmp > 0) current = current.right;
+            else {
+                current.val = val;
+                return;
+            }
+        }
+        Node newNode = new Node(key, val);
+        if (cmp < 0) parent.left = newNode;
+        else parent.right = newNode;
+        size++;
+    }
 
     public V get(K key) {
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+            if (cmp < 0) current = current.left;
+            else if (cmp > 0) current = current.right;
+            else return current.val;
+        }
         return null;
     }
 
-    public void delete(K key) {}
-
-    public Iterable<K> iterator() {
-        return null;
+    public void delete(K key) {
+        Node current = root;
+        Node parent = null;
+        while (current != null && !current.key.equals(key)) {
+            parent = current;
+            if (key.compareTo(current.key) < 0) current = current.left;
+            else current = current.right;
+        }
+        if (current == null) return;
+        if (current.left != null && current.right != null) {
+            Node successor = current.right;
+            Node successorParent = current;
+            while (successor.left != null) {
+                successorParent = successor;
+                successor = successor.left;
+            }
+            current.key = successor.key;
+            current.val = successor.val;
+            parent = successorParent;
+            current = successor;
+        }
+        Node child = (current.left != null) ? current.left : current.right;
+        if (parent == null) root = child;
+        else if (parent.left == current) parent.left = child;
+        else parent.right = child;
+        size--;
     }
 
-
+    @Override
+    public Iterator<Element<K, V>> iterator() {
+        List<Element<K, V>> list = new ArrayList<>();
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            list.add(new Element<>(current.key, current.val));
+            current = current.right;
+        }
+        return list.iterator();
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,40 @@ public class Main {
         int buckets = 100;
         MyHashTable<MyTestingClass, Student> table = new MyHashTable<>(buckets);
         Random rnd = new Random();
+        System.out.println("Testing HashTable");
+        System.out.println();
         for (int i = 0; i < 10000; i++) {
             int key = rnd.nextInt(1000000);
             table.put(new MyTestingClass(key), new Student("Name" + i, rnd.nextInt(30) + 18));
         }
         for (int i = 0; i < buckets; i++) {
             System.out.println("Bucket " + i + ": " + table.bucketSize(i) + " elements");
+        }
+
+        System.out.println();
+        System.out.println("-----------------------------------------------");
+        System.out.println("Testing BST");
+
+        BST<Integer, String> tree = new BST<>();
+        tree.put(10, "ten");
+        tree.put(5, "five");
+        tree.put(15, "fifteen");
+        tree.put(3, "three");
+        tree.put(7, "seven");
+
+        System.out.println("BST Size: " + tree.size());
+        System.out.println("Value for key 7: " + tree.get(7));
+
+        System.out.println("BST In-order Traversal:");
+        for (BST.Element<Integer, String> elem : tree) {
+            System.out.println("key = " + elem.getKey() + ", value = " + elem.getValue());
+        }
+
+        tree.delete(5);
+        System.out.println("After deleting key 5, BST Size: " + tree.size());
+        System.out.println("In-order after deletion:");
+        for (BST.Element<Integer, String> elem : tree) {
+            System.out.println("key = " + elem.getKey() + ", value = " + elem.getValue());
         }
     }
 }
